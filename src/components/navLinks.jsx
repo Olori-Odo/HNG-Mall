b5df02e51ac6042173f6981edfe4878b90a48ad2
Fatai Akinyemi <ojuotimi04@gmail.com>
2024-07-10 00:01:20 +0100

    commit

--- src/components/navLinks.jsx
+++ src/components/navLinks.jsx
@@ -43,6 +43,23 @@ const MainLayout = () => {
                   </li>
                 </ul>
               </li> */}
+
+      {/* import { defineConfig } from "vite"; */}
+      {/* import { version } from "./package.json";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].${version}.js`,
+        chunkFileNames: `[name].${version}.js`,
+        assetFileNames: `[name].${version}.[ext]`,
+      },
+    },
+  },
+}); */}
+
       <Outlet />
     </>
   );
--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
-// import { defineConfig } from "vite";
-import { version } from "./package.json";
-
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name].${version}.js`,
-        chunkFileNames: `[name].${version}.js`,
-        assetFileNames: `[name].${version}.[ext]`,
-      },
-    },
-  },
 });
 
 // export default {
